d48425d60a50a7d427a9ad09effffd0bcd1c621d
gaiiaa15 <183663308+gaiiaa15@users.noreply.github.com>
2026-08-25 18:58:15 +0100

    Define typed solar calculation parameters

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+interface CalculationParameters {
+    panelCount: number;
+    panelWattage: number;
+    peakSunHours: number;
+    electricityPriceGbpPerKwh: number;
+}
 
+const exampleCalculations: CalculationParameters = {
+    panelCount:6,
+    panelWattage:350,
+    peakSunHours:4.5,
+    electricityPriceGbpPerKwh:0.2611
 
-console.log('Happy developing ✨')
+};
+console.log(exampleCalculations);
