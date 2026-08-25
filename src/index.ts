77a14c5b450c43ec610ed51ec8449a8e2b69dbf3
gaiiaa15 <183663308+gaiiaa15@users.noreply.github.com>
2026-08-25 19:55:18 +0100

    Daily Energy Generation in kWh calculation done

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,16 @@ const exampleCalculations: CalculationParameters = {
     panelCount:6,
     panelWattage:350,
     peakSunHours:4.5,
-    electricityPriceGbpPerKwh:0.2611
+    electricityPriceGbpPerKwh:0.2611,
 
 };
-console.log(exampleCalculations);
+
+// calculates the daily energy generated in kWh
+function dailyEnergyGenerationInKwh
+(input : CalculationParameters) : number{
+  let dailyEnergy : number = 0;
+  dailyEnergy = (input.panelCount * input.panelWattage) * input.peakSunHours / 1000;
+  return dailyEnergy;
+
+}
+console.log(dailyEnergyGenerationInKwh(exampleCalculations) + "kWh");
